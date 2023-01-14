fd83902836e2072d1e286f3d58f99bc03991e7d9
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-01-14 16:44:16 +0600

    initial Setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
+import ProductCard from './../components/ProductCard';
 
 const Home = () => {
 
-  const [product, setProduct] = useState([]);
+  const [products, setProducts] = useState([]);
   useEffect(()=>{
     fetch("products.json")
     .then((res)=> res.json())
-    .then(data=> setProduct(data))
+    .then(data=> setProducts(data))
   },[]);
-  console.log(product);
+  console.log(products);
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+      {
+        products.map(product =><ProductCard key={product._id} product={product}/>)
+      }
     </div>
   );
 };
