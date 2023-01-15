3b3e54c07cd9125149f7e0ec2fe892285bf69b8a
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-01-15 10:37:57 +0600

    Redux Devtool integration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import productReducer from './reducers/productReducers';
 
 
-const store = createStore(productReducer);
+const store = createStore(productReducer,composeWithDevTools());
 
 export default store;
